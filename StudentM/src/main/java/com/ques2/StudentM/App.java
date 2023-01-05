b0ec1a1125882917fb61b850a25a539a813680b7
mrrajput12 <115772498+mrrajput12@users.noreply.github.com>
2023-01-05 13:42:56 +0530

    Add files via upload

---
+++ StudentM/src/main/java/com/ques2/StudentM/App.java
@@ -0,0 +1,38 @@
+package com.ques2.StudentM;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg = cfg.configure();
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction tr = s.beginTransaction();
+//		Student Stu1 = new Student();
+//		Stu1.setStu_id(1);
+//		Stu1.setStu_name("Abhishek");
+//		Stu1.setStu_course("java");
+//		Stu1.setStu_Admission(2020);
+//
+//		Student Stu2 = new Student();
+//	    Stu2.setStu_id(2);
+//   	Stu2.setStu_name("Rahul");
+//	    Stu2.setStu_course(".net");
+//	    Stu2.setStu_Admission(2021);
+//
+//		s.save(Stu1);
+//	    s.save(Stu2);
+//		tr.commit();
+//		System.out.println("object saved");
+//		System.out.println(s.get(Student.class, 1));
+//		System.out.println(s.get(Student.class, 2));
+		Student Stu = s.get(Student.class, 1);
+		s.delete(Stu);
+		tr.commit();
+
+	}
+}
